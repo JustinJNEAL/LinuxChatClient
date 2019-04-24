78889256aba61f583137b1ece81116240c2113b3
Justin <justinjneal@protonmail.com>
2019-04-24 13:51:46 -0400

    Updated server and client file. Minor hiccups still exist.

--- client.c
+++ client.c
@@ -1,6 +1,5 @@
 /*
-* ./client [server ip] [PORT]
-* 
+* ./client [host ip] [username]
 */
 
 #include <stdio.h>
@@ -10,85 +9,172 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <netdb.h>
 
-#define CHARNUM 159
+#define CHARNUM 160
 #define USRCAP 10
-#define PORT 8080
+#define PORT 3333
 
 void error(const char *warning)
 {
     perror(warning);
     exit(0);
 }
+/*
+void * createSocket(int fd)
+{
+    fd = socket(PF_INET, SOCK_STREAM, 0);
+    if (fd < 0)
+    {
+        error("Error opening socket.");
+    }
+    return 0;
+}
+*/
+void * connectServer(int sockfd, struct sockaddr_in *s_address)
+{
+    if ((connect(sockfd, (struct sockaddr *) s_address, sizeof(*s_address))) < 0)
+    {
+        error("Connection failed.");
+    }
+    else
+    {
+        puts("Connected.");
+    }
+}
+
+void * recvMessage(int sockfd, char *response)
+{
+    int recvSize;
+    char buffer[CHARNUM];
+    memset(buffer, 0, CHARNUM);
+
+    while(1)
+    {
+        recvSize = recv(sockfd, buffer, CHARNUM, 0);
+        if (recvSize < 0)
+        {
+            error("Error reading");
+        }
+        else if (recvSize == 0)
+        {
+            puts("Client disconnect.");
+        }
+        else
+        {
+            printf("\nServer: %s", buffer);
+            printf("%s", response);
+            fflush(stdout);
+        }
+    }
+    return 0;
+}
+
+void * sendMessage(char s_response[1000], int sockfd, struct sockaddr_in *s_address)
+{
+    printf("%s", s_response);
+    char buffer[CHARNUM], fnl[2000];
+
+    while(fgets(buffer, CHARNUM, stdin) != NULL)
+    {
+        memset(fnl, 0, 2000);
+        strcat(fnl, s_response);
+        strcat(fnl, buffer);
+        printf("\n%s", s_response);
+
+        if (strncmp(buffer, ":exit", 5) == 0) {
+            error("Disconnected");
+        }
+
+        send(sockfd, fnl, strlen(fnl) + 1, 0);
+    }
+    return 0;
+}
+
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portNum, n;
+    int sockfd, n;
     struct sockaddr_in server_addr;
     struct hostent *server;
-    char buffer[CHARNUM], username[USRCAP], s_response[1000];
+    char buffer[CHARNUM], username[USRCAP], s_response[1000], fnl[2000];
 
-    if (argc < 2)
+    // Check for correct amount of line arguments
+    if (argc < 3)
     {
-        fprintf(stderr, "Usage %s [HOST IP]\n", argv[0]);
+        fprintf(stderr, "Usage: %s [HOST IP] [USERNAME]\n", argv[0]);
         exit(1);
     }
 
-    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    // Checks if username is too long
+    if (strlen(argv[2]) >= sizeof(username))
+    {
+        error("Username is too long.");
+    }
+    strcpy(username, argv[2]);
+
+    username[strlen(username) - 1] = 0;
+    strncpy(s_response, username, strlen(username));
+    strcat(s_response, ": ");
+
+    // Step 1
+    // Create socket
+    sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
     {
         error("Error opening socket.");
     }
 
+    // Setting 2nd line argument to equal host ip
     server = gethostbyname(argv[1]);
     if (server == NULL)
     {
         fprintf(stderr, "Error. Host does not exist.");
     }
 
+
+    // Clear and set server address
     memset((char *) &server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     memcpy((char *) &server_addr.sin_addr.s_addr, (char *) server->h_addr_list[0], server->h_length);
+    // Set server port
     server_addr.sin_port = htons(PORT);
 
+    // Step 2
     if (connect(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
     {
-        error("Connection failed");
+        error("Connection failed.");
+    }
+    else
+    {
+        puts("Connected.");
     }
 
-    printf("Who are you?\n");
-    memset(username, '\0', sizeof(username));
-    fgets(username, sizeof(username), stdin);
-    if ((send(sockfd, username, strlen(username), 0)) < 0)
+    send(sockfd, username, strlen(username), 0);
+
+    while(1)
     {
-        error("Error writing.");
-    }  
-    
-    while (1)
-    {
-        memset(buffer, '\0', sizeof(buffer));
-        fgets(buffer, sizeof(buffer), stdin);
-        memset(s_response, '\0', sizeof(s_response));
-
-        if ((send(sockfd, buffer, strlen(buffer), 0)) < 0)
+        memset(buffer, 0, CHARNUM);
+        memset(fnl, 0, strlen(fnl));
+        fgets(buffer, CHARNUM, stdin);
+        if (strncmp(buffer, ":exit", 5) == 0)
         {
-            error("Error writing.");
-        }
-
-        if ((recv(sockfd, s_response, sizeof(s_response), 0)) < 0)
-        {
-            error("Error reading.");
-        }
-
-        printf("%s: %s", username, s_response);
-
-        int i = strncmp(":exit", buffer, 5);
-        if (i == 0)
             break;
-        
-    }
+        }
+        strcat(fnl, s_response);
+        strcat(fnl, buffer);
+        if (send(sockfd, fnl, strlen(fnl), 0) < 0)
+        {
+            perror("Writing failed");
+        }
 
+        if (recv(sockfd, fnl, strlen(fnl), 0) < 0)
+        {
+            perror("Reading failed");
+        }
+        puts(fnl);
+    }
+    
     close(sockfd);
     
     return 0;
--- server.c
+++ server.c
@@ -1,6 +1,6 @@
 /*
 * To run server, first compile the program then run this command in the terminal:
-*   ./server [PORT]
+*   ./server
 *
 */
 
@@ -11,187 +11,139 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <pthread.h>
 
-#define CHARNUM 159
+#define CHARNUM 160
 #define USRCAP 10
 #define MAX_CLIENT 5
-#define PORT 8080
+#define PORT 3333
 
-// Error function that takes message as parameter
+// Error function that takes warning message as parameter
 void error(const char *warning)
 {
     perror(warning);
     exit(1);
 }
 
-void *handler(void *);
+void * recvMessage(int fd)
+{
+    int recvSize;
+    char buffer[CHARNUM];
 
+    memset(buffer, 0, CHARNUM);
+
+    while(1)
+    {
+        recvSize = recv(fd, buffer, CHARNUM, 0);
+        if (recvSize)
+        {
+            printf("%s", buffer);
+        }
+    }
+    return 0;
+}
+
+void * sendMessage(int newfd/*, struct sockaddr *c_address*/)
+{
+    char buffer[CHARNUM];
+
+    while(fgets(buffer, CHARNUM, stdin) != NULL)
+    {
+        if (strncmp(buffer, ":exit", 5) == 0)
+        {
+            error("Disconnected.");
+        }
+        send(newfd, buffer, CHARNUM, 0);
+    }
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
-    int sockfd, newsockfd, *sock, n, yes = 1;
-    char buffer[CHARNUM], username[USRCAP];
+    int sockfd, newsockfd, yes = 1, recvSize;
+    char buffer[CHARNUM], username[USRCAP], fnl[2000], usrGreeting[40];
     struct sockaddr_in server_addr, client_addr;
     socklen_t clientLen;
 
+    // Step 1
     // Create socket
     sockfd = socket(PF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
     {
-        // Print socket connection error
+        // Socket connection error
         error("Error opening socket.");
     }
-    puts("...Socket created");
+    else if (sockfd > 0)
+    {
+        // Socket connection success
+        puts("... Socket created");
+    }
 
+    // Clear server address
     memset((char *) &server_addr, 0, sizeof(server_addr));
-
+    // Declare server address fields
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
 
+    // Step 2
+    // Set socket option (for reuse of port immediately after disconnection)
     setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
     if (bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
     {
-        // Print binding error
         error("Binding failed.");
     }
-    puts("...Bind successful");
+    else
+    {
+        puts("... Bind successful");
+    }
     
     // Listen for connection, allow maximum of 5 clients
     listen(sockfd, MAX_CLIENT);
-    clientLen = sizeof(client_addr);
     // Lets users see that the server is on and listening
     puts("Waiting for incoming connections...");
     
-    while ((newsockfd = accept(sockfd, (struct sockaddr *) &client_addr, (socklen_t*)&clientLen)) > 0)
+    clientLen = sizeof(client_addr);
+    // Step 3
+    // Accept new connections
+    newsockfd = accept(sockfd, (struct sockaddr *) &client_addr, &clientLen);
+    if (newsockfd < 0)
+    {
+        error("Connection could not be established.");
+    }
+    else
     {
         puts("...Connection established");
-        
-        int fd = *(int*)sockfd;
-    int recvSize;
-    char buffer[CHARNUM], username[USRCAP], *msg, usrGreeting[31], s_response[1000];
-    
-    memset(username, '\0', sizeof(username));
-    if ((recv(fd, username, sizeof(username), 0)) < 0)
-    {
-        error("Error reading.");
     }
-    
-    memset(usrGreeting, '\0', sizeof(usrGreeting));
-    
-    
-  
 
-    msg = "Now go on and chat to your heart's content!\n";
-    send(fd, msg, strlen(msg), 0);
-
-    // Clear buffer
-    
-    // Recieve buffer
-
-    while ((recvSize = recv(fd, buffer, sizeof(buffer), 0)) > 0)
+    if ((recv(newsockfd, username, sizeof(username), 0)) < 0)
     {
-        // Concatenate fbuffer
-        memset(s_response, '\0', sizeof(s_response));
-        strncpy(s_response, username, strlen(username));
-        strncat(s_response, ": ", strlen(": "));
-        strncat(s_response, buffer, sizeof(buffer));
+        perror("Read failed");
+    }
 
-        // Send response to client
-        send(fd, s_response, strlen(s_response), 0);
+    username[strlen(username) - 1] = 0;
+    memset(usrGreeting, 0, sizeof(usrGreeting));
+    strcpy(usrGreeting, "Welcome ");
+    strncat(usrGreeting, username, strlen(username));
+    strcat(usrGreeting, " to the chat!");
+    // Send greeting to client
+    send(newsockfd, usrGreeting, strlen(usrGreeting), 0);
 
-        // Clear buffer
-        memset(buffer, '\0', sizeof(buffer));
-        memset(s_response, '\0', sizeof(s_response));
-        
-        // Initialize i to compare the first 5 bytes in buffer to ':exit'
-        int i = strncmp(":exit", buffer, 5);
-        // If i == 0, end connection
-        if (recvSize == 0 || i == 0)
+    memset(fnl, 0, sizeof(fnl));
+    while((recvSize = recv(newsockfd, fnl, sizeof(buffer), 0)) > 0)
+    {
+        if (send(newsockfd, fnl, strlen(fnl), 0) < 0)
         {
-            printf("%s has left the chat\n", username);
+            perror("Writing failed");
         }
-    
     }
     
-    close(fd);
-    free(sockfd);
-    }
+
+   
+    close(newsockfd);
+    close(sockfd);
     
     return 0;
 }
-
-void * sendGreeting(int fd, char usr)
-{
-    char usrGreeting[31];
-
-    strncpy(usrGreeting, "Welcome ", 8);
-    strncat(usrGreeting, usr, strlen(usr));
-    strncat(usrGreeting, " to the chat!", sizeof(" to the chat!"));
-    if (send(fd, usrGreeting, sizeof(usrGreeting), 0) < 0)
-    {
-        error("Error writing");
-    }
-}
-
-void *handler(void *sockfd)
-{
-    int fd = *(int*)sockfd;
-    int recvSize;
-    char buffer[CHARNUM], username[USRCAP], *msg, usrGreeting[31], s_response[1000];
-    
-    memset(username, '\0', sizeof(username));
-    if ((recv(fd, username, sizeof(username), 0)) < 0)
-    {
-        error("Error reading.");
-    }
-    
-    memset(usrGreeting, '\0', sizeof(usrGreeting));
-    
-    
-  
-
-    msg = "Now go on and chat to your heart's content!\n";
-    send(fd, msg, strlen(msg), 0);
-
-    // Clear buffer
-    
-    // Recieve buffer
-
-    while ((recvSize = recv(fd, buffer, sizeof(buffer), 0)) > 0)
-    {
-        // Concatenate fbuffer
-        memset(s_response, '\0', sizeof(s_response));
-        strncpy(s_response, username, strlen(username));
-        strncat(s_response, ": ", strlen(": "));
-        strncat(s_response, buffer, sizeof(buffer));
-
-        // Send response to client
-        send(fd, s_response, strlen(s_response), 0);
-
-        // Clear buffer
-        memset(buffer, '\0', sizeof(buffer));
-        memset(s_response, '\0', sizeof(s_response));
-        
-        // Initialize i to compare the first 5 bytes in buffer to ':exit'
-        int i = strncmp(":exit", buffer, 5);
-        // If i == 0, end connection
-        if (recvSize == 0 || i == 0)
-        {
-            printf("%s has left the chat\n", username);
-        }
-    
-    }
-    
-    close(fd);
-    free(sockfd);
-
-    return 0;
-}
-
-
-
 
