e193db5acafdfefd9a49d2b2adfe67623aeb7fec
kelvinblojay <kblojay1@student.gsu.edu>
2019-04-25 12:41:34 -0400

    Update write.c

--- write.c
+++ write.c
@@ -11,10 +11,10 @@ main() {
 void write(char str[50]){//function opens and write string to 
 //passed as and argument to a fileS
 	
-	FILE *fp;
-	fp = fopen("chatroom.txt", "a");
-	fprintf(fp, str);
-	fprintf(fp,"\n");
-	fclose(fp);
+	FILE *fp;// create a pointer object of the type File
+	fp = fopen("chatroom.txt", "a");//The File oject contains a fopen() function that accepts two argument: filename and mode
+	fprintf(fp, str);//fprintf() function to write to the file: accepts the File pointer object and the string to be added
+	fprintf(fp,"\n");//prints a new line to the file for next insertion
+	fclose(fp);// closes up the file
 
 }//write function ends
