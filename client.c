e23a56a15cc37ef28e4ff9a676701219f34f13db
Justin <justinjneal@protonmail.com>
2019-04-25 11:58:29 -0400

    Fixed string cat errors

--- client.c
+++ client.c
@@ -1,5 +1,5 @@
 /*
-* ./client [host ip] [username]
+* ./client [host ip]
 */
 
 #include <stdio.h>
@@ -20,102 +20,29 @@ void error(const char *warning)
     perror(warning);
     exit(0);
 }
-/*
-void * createSocket(int fd)
-{
-    fd = socket(PF_INET, SOCK_STREAM, 0);
-    if (fd < 0)
-    {
-        error("Error opening socket.");
-    }
-    return 0;
-}
-*/
-void * connectServer(int sockfd, struct sockaddr_in *s_address)
-{
-    if ((connect(sockfd, (struct sockaddr *) s_address, sizeof(*s_address))) < 0)
-    {
-        error("Connection failed.");
-    }
-    else
-    {
-        puts("Connected.");
-    }
-}
-
-void * recvMessage(int sockfd, char *response)
-{
-    int recvSize;
-    char buffer[CHARNUM];
-    memset(buffer, 0, CHARNUM);
-
-    while(1)
-    {
-        recvSize = recv(sockfd, buffer, CHARNUM, 0);
-        if (recvSize < 0)
-        {
-            error("Error reading");
-        }
-        else if (recvSize == 0)
-        {
-            puts("Client disconnect.");
-        }
-        else
-        {
-            printf("\nServer: %s", buffer);
-            printf("%s", response);
-            fflush(stdout);
-        }
-    }
-    return 0;
-}
-
-void * sendMessage(char s_response[1000], int sockfd, struct sockaddr_in *s_address)
-{
-    printf("%s", s_response);
-    char buffer[CHARNUM], fnl[2000];
-
-    while(fgets(buffer, CHARNUM, stdin) != NULL)
-    {
-        memset(fnl, 0, 2000);
-        strcat(fnl, s_response);
-        strcat(fnl, buffer);
-        printf("\n%s", s_response);
-
-        if (strncmp(buffer, ":exit", 5) == 0) {
-            error("Disconnected");
-        }
-
-        send(sockfd, fnl, strlen(fnl) + 1, 0);
-    }
-    return 0;
-}
 
 
 int main(int argc, char *argv[])
 {
-    int sockfd, n;
+    int sockfd;
     struct sockaddr_in server_addr;
     struct hostent *server;
     char buffer[CHARNUM], username[USRCAP], s_response[1000], fnl[2000];
 
     // Check for correct amount of line arguments
-    if (argc < 3)
+    if (argc < 2)
     {
-        fprintf(stderr, "Usage: %s [HOST IP] [USERNAME]\n", argv[0]);
+        fprintf(stderr, "Usage: %s [HOST IP]\n", argv[0]);
         exit(1);
     }
 
-    // Checks if username is too long
-    if (strlen(argv[2]) >= sizeof(username))
-    {
-        error("Username is too long.");
-    }
-    strcpy(username, argv[2]);
+    printf("What's your name?\n");
+    fgets(username, USRCAP, stdin);
+    memset(fnl, 0, sizeof(fnl));
+    strcpy(fnl, username);
+    strcat(fnl, ": ");
 
-    username[strlen(username) - 1] = 0;
-    strncpy(s_response, username, strlen(username));
-    strcat(s_response, ": ");
+    printf("Welcome to the chat, %s\n", username);
 
     // Step 1
     // Create socket
@@ -145,23 +72,15 @@ int main(int argc, char *argv[])
     {
         error("Connection failed.");
     }
-    else
-    {
-        puts("Connected.");
-    }
-
-    send(sockfd, username, strlen(username), 0);
 
     while(1)
     {
         memset(buffer, 0, CHARNUM);
-        memset(fnl, 0, strlen(fnl));
         fgets(buffer, CHARNUM, stdin);
         if (strncmp(buffer, ":exit", 5) == 0)
         {
             break;
         }
-        strcat(fnl, s_response);
         strcat(fnl, buffer);
         if (send(sockfd, fnl, strlen(fnl), 0) < 0)
         {
@@ -174,7 +93,7 @@ int main(int argc, char *argv[])
         }
         puts(fnl);
     }
-    
+
     close(sockfd);
     
     return 0;
