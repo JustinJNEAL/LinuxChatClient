2f62de96870b23193c27c779e0b9777ec50bbb51
JustinJNEAL <45116043+JustinJNEAL@users.noreply.github.com>
2019-04-25 12:37:22 -0400

    Update Pclient.c

--- Pclient.c
+++ Pclient.c
@@ -30,10 +30,7 @@ void* Write(void* arg){
 		stpcpy(temp,user);
 		memset(buffer1, 0, sizeof(buffer1));
 		fgets(buffer1, sizeof(buffer1), stdin);
-		if (strncmp(buffer1, ":exit", 5) == 0)
-		{
-			break;
-		}
+		
 		strcat(temp,":");
 		strcat(temp,buffer1);
 		//stpcpy(buffer1,temp);
@@ -41,6 +38,10 @@ void* Write(void* arg){
 	
 		if (n < 0){
 			error("Error writing");
+		}
+		if (strncmp(buffer1, ":exit", 5) == 0)
+		{
+			break;
 		}
     
 		 
