d94ae0a986b83aeb5e9c6c5c347bffe698530c18
Justin <justinjneal@protonmail.com>
2019-04-27 01:30:10 -0400

    Updates

--- Pclient.c
+++ Pclient.c
@@ -24,36 +24,35 @@ void error(const char *warning)
     exit(0);
 }
 
-void* Write(void* arg)
+void *Write(void *arg)
 {
-	while (1)
-    {	
-		char temp[CHARNUM+10];
-		stpcpy(temp,user);
-		memset(buffer1, 0, sizeof(buffer1));
-		fgets(buffer1, sizeof(buffer1), stdin);
+    while (1)
+    {
+        char temp[CHARNUM + 10];
+        stpcpy(temp, user);
+        memset(buffer1, 0, sizeof(buffer1));
+        fgets(buffer1, sizeof(buffer1), stdin);
         if (strncmp(buffer1, ":exit", 5) == 0)
         {
             puts("Program terminated");
             exit(0);
         }
-        
-		strcat(temp,":");
-		strcat(temp,buffer1);
-		//stpcpy(buffer1,temp);
-		n = send(sockfd, temp, strlen(temp), 0);
-	
-		if (n < 0){
-			error("Error writing");
-		}
-		
-    
-		 
+
+        strcat(temp, ":");
+        strcat(temp, buffer1);
+        //stpcpy(buffer1,temp);
+        n = send(sockfd, temp, strlen(temp), 0);
+
+        if (n < 0)
+        {
+            error("Error writing");
+        }
     }
     pthread_exit(0);
 }
-void* Read(void* arg){
-	while (1)
+void *Read(void *arg)
+{
+    while (1)
     {
         memset(buffer2, 0, sizeof(buffer2));
         n = recv(sockfd, buffer2, sizeof(buffer2), 0);
@@ -64,23 +63,22 @@ void* Read(void* arg){
         }
 
         printf("%s", buffer2);
-		
+
         int i = strncmp(ExitStr, buffer2, strlen(ExitStr));
 
-        if (i == 0){
+        if (i == 0)
+        {
             break;
         }
     }
     pthread_exit(0);
 }
 
-
 int main(int argc, char *argv[])
 {
     struct sockaddr_in server_addr;
     struct hostent *server;
 
-    
     if (argc < 3)
     {
         fprintf(stderr, "Usage: %s [HOST IP] [USERNAME]\n", argv[0]);
@@ -89,7 +87,6 @@ int main(int argc, char *argv[])
 
     memset(user, 0, sizeof(user));
     strcpy(user, argv[2]);
-    
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
@@ -103,32 +100,32 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Error. Host does not exist.");
     }
 
-    memset((char *) &server_addr, 0, sizeof(server_addr));
+    memset((char *)&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    memcpy((char *) &server_addr.sin_addr.s_addr, (char *) server->h_addr_list[0], server->h_length);
+    memcpy((char *)&server_addr.sin_addr.s_addr, (char *)server->h_addr_list[0], server->h_length);
     server_addr.sin_port = htons(PORT);
 
-    if (connect(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     {
         error("Connection failed");
     }
 
-    stpcpy(ExitStr,user);
-	strcat(ExitStr,":exit");
-	pthread_t tid[2];
+    stpcpy(ExitStr, user);
+    strcat(ExitStr, ":exit");
+    pthread_t tid[2];
 
     pthread_attr_t attr;
     pthread_attr_init(&attr);
 
-    pthread_create(&tid[0], &attr,Write,NULL);
-    pthread_create(&tid[1], &attr,Read,NULL);
+    pthread_create(&tid[0], &attr, Write, NULL);
+    pthread_create(&tid[1], &attr, Read, NULL);
 
-	for(int j = 0; j < 2; j++)
+    for (int j = 0; j < 2; j++)
     {
-		pthread_join(tid[j],NULL);
-	}
+        pthread_join(tid[j], NULL);
+    }
 
     close(sockfd);
-    
+
     return 0;
 }
--- PserverV2.c
+++ PserverV2.c
@@ -1,4 +1,4 @@
-#include <stdio.h>   // Input & output
+#include <stdio.h> // Input & output
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -15,61 +15,64 @@
 char buffer1[CHARNUM];
 int sockfd, n, newsockfd, temp, sockhd[MAX_CLIENT], CountOnlineUser;
 
-void error(const char *warning){
-    perror(warning);
-    exit(1);
+void error(const char *warning)
+{
+	perror(warning);
+	exit(1);
 }
 
-void* Write(int* arg){
-	int* temp = (int*) arg;
-	int newTemp=*temp;
+void *Write(int *arg)
+{
+	int *temp = (int *)arg;
+	int newTemp = *temp;
 
-	while (1){	
-		
+	while (1)
+	{
+
 		memset(buffer1, 0, sizeof(buffer1));
 		if (recv(sockhd[newTemp - 1], buffer1, sizeof(buffer1), 0) < 0)
 		{
 			perror("Error reading");
 		}
 		int j = 0;
-		for(j = 0; j < CountOnlineUser; j++){
+		for (j = 0; j < CountOnlineUser; j++)
+		{
 			//n = send(sockhd[j], buffer1, strlen(buffer1)
 			if (send(sockhd[j], buffer1, strlen(buffer1), 0) < 0)
 			{
 				perror("Error writing");
 			}
-    	}	   
-    }
-    pthread_exit(0);
+		}
+	}
+	pthread_exit(0);
 }
-
 
 int main(int argc, char *argv[])
 {
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t clientLen;
+	struct sockaddr_in server_addr, client_addr;
+	socklen_t clientLen;
 
-    // Create socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-    {
-        // Print socket connection error
-        error("Error opening socket.");
-    }
+	// Create socket
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+	{
+		// Print socket connection error
+		error("Error opening socket.");
+	}
 
-    memset((char *) &server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
+	memset((char *)&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_port = htons(PORT);
 
 	// Set socket option (for reuse of port immediately after disconnection)
 	int yes = 1;
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
-    if (bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
-    {
-        // Print binding error
-        error("Binding failed");
-    }
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+	if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+	{
+		// Print binding error
+		error("Binding failed");
+	}
 
 	pthread_t tid[MAX_CLIENT];
 	pthread_attr_t attr;
@@ -78,35 +81,35 @@ int main(int argc, char *argv[])
 	// Lets users see that the server is on and listening
 	puts("Waiting for incoming connections...");
 
-    for(int i = 0; i < MAX_CLIENT; i++)
+	for (int i = 0; i < MAX_CLIENT; i++)
 	{
 		// Listen for connection, allow maximum of 5 clients
 		listen(sockfd, MAX_CLIENT);
 		clientLen = sizeof(client_addr);
 		//long long V=(long long)i;
-		newsockfd = accept(sockfd, (struct sockaddr *) &client_addr, &clientLen);
-	    sockhd[i]=newsockfd;
-		
+		newsockfd = accept(sockfd, (struct sockaddr *)&client_addr, &clientLen);
+		sockhd[i] = newsockfd;
+
 		if (newsockfd < 0)
 		{
 			// Print accepting error
 			error("Error accepting.");
 		}
-		
-		pthread_create(&tid[i], &attr,Write,&i);
+
+		pthread_create(&tid[i], &attr, Write, &i);
 		int *ptr = 100;
 		wait(ptr);
-	    CountOnlineUser++;
-	    
+		CountOnlineUser++;
 	}
 
-	int j=0;
-	for(j=0;j<MAX_CLIENT;j++){
-		pthread_join(tid[j],NULL);
+	int j = 0;
+	for (j = 0; j < MAX_CLIENT; j++)
+	{
+		pthread_join(tid[j], NULL);
 	}
 
-    close(newsockfd);
-    close(sockfd);
-    
-    return 0;
+	close(newsockfd);
+	close(sockfd);
+
+	return 0;
 }
